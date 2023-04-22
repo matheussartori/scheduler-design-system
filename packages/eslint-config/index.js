5b87b6f1079d23b81066f32d003c25a34e515c84
Matheus Sartori <ms.sartori@outlook.com.br>
2023-04-22 19:14:50 -0300

    chore: add eslint rules

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,3 +1,29 @@
 module.exports = {
-  extends: ['@rocketseat/eslint-config/react']
+  extends: ['@rocketseat/eslint-config/react'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'comma-dangle': [
+      'error',
+      'never'
+    ],
+    'arrow-parens': [
+      0
+    ],
+    'space-before-function-paren': [
+      'error',
+      'never'
+    ],
+  }
 }
