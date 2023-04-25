ec80d759e5580b0daf430e15780be210909684bc
Matheus Sartori <ms.sartori@outlook.com.br>
2023-04-24 22:41:04 -0300

    feat: multistep component

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, MultiStep, type MultiStepProps } from '@matheussartori/scheduler-design-system-react'
+
+export default {
+  title: 'Form/MultiStep',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1
+  },
+  decorators: [
+    (Story) => {
+      return (
+        <Box as="label" css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}>
+          {Story()}
+        </Box>
+      )
+    }
+  ]
+} as Meta<MultiStepProps>
+
+export const Primary: StoryObj<MultiStepProps> = {}
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4
+  }
+}
---
+++ packages/react/src/components/MultiStep/index.tsx
@@ -0,0 +1,24 @@
+import { type ReactNode } from 'react'
+import { Label, MultiStepContainer, Step, Steps } from './styles'
+
+export interface MultiStepProps {
+  size: number
+  currentStep?: number
+}
+
+export function MultiStep ({
+  size,
+  currentStep = 1
+}: MultiStepProps): ReactNode {
+  return (
+    <MultiStepContainer>
+      <Label>Passo {currentStep} de {size}</Label>
+
+      <Steps css={{ '--steps-size': size }}>
+        {Array.from({ length: size }, (_, i) => i + 1).map(step => {
+          return <Step key={step} active={currentStep >= step} />
+        })}
+      </Steps>
+    </MultiStepContainer>
+  )
+}
